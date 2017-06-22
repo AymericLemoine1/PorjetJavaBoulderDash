8301de63d562bff3dcf0ee32bd39be2113050a2f
AymericLemoine <aymeric.lemoine@viacesi.fr>
2017-06-22 14:34:08 +0200

    creation maven

---
+++ ProjetJavaMaven/src/main/java/fr/exia/gr7/ProjetJavaMaven/App.java
@@ -0,0 +1,13 @@
+package fr.exia.gr7.ProjetJavaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
